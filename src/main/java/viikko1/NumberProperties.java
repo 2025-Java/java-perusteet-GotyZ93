bc9994e7b2cd17af2ec817fccf5efcf6f55ce435
GotyZ93 <mauri.olli93@gmail.com>
2025-09-07 20:51:05 +0300

    Number properties

--- src/main/java/viikko1/NumberProperties.java
+++ src/main/java/viikko1/NumberProperties.java
@@ -2,7 +2,22 @@ package viikko1;
 
 public class NumberProperties {
     public static String describe(int n) {
-        // TODO
-        return null;
+
+    if(n>0 && n% 2==0){
+            return "positiivinen parillinen";
+        }
+        else if(n>0 && n%2==1){
+            return "positiivinen pariton";
+        }
+        else if(n<0&&n%2==0){
+            return "negatiivinen parillinen";
+        }
+        else if(n<0&&n%2==1){
+            return "negatiivinen pariton";
+        }
+        else {
+            return "nolla";
+        }
+        
     }
 }
