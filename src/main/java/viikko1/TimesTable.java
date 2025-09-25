7cf2714a1ee1839a9718ecf81134e50f068a9c30
GotyZ93 <mauri.olli93@gmail.com>
2025-09-25 20:17:07 +0300

    Push TimesTable

--- src/main/java/viikko1/TimesTable.java
+++ src/main/java/viikko1/TimesTable.java
@@ -2,6 +2,15 @@ package viikko1;
 
 public class TimesTable {
     public static String table(int n) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i=1; i <=10; i++){
+         sb.append(n).append(" x ").append(i).append(" = ").append(n * i);
+         if (i<=10){
+            sb.append("\n");
+         }
+        }
+
+        return sb.toString();
+        
     }
 }
