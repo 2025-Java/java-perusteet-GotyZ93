d1c251e4204c5a480f07c211ecbcd3770dcc2d24
GotyZ93 <mauri.olli93@gmail.com>
2025-09-25 19:52:56 +0300

    Push SortThree

--- src/main/java/viikko1/SortThree.java
+++ src/main/java/viikko1/SortThree.java
@@ -3,9 +3,9 @@ package viikko1;
 
 public class SortThree {
     public static String ascending(int a, int b, int c) {
-        double max = Math.max(a, Math.max(b, c));
-        double min = Math.min(a, Math.min(b, c));
-        double mid = a+b+c-max-min;
+        int max = Math.max(a, Math.max(b, c));
+        int min = Math.min(a, Math.min(b, c));
+        int mid = a+b+c-max-min;
         
         return ""+min+","+mid+","+max;
     }
